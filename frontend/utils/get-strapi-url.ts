90b89aae8f429b04d33107323bea592fede44724
Oktavian Paunku <oggy134@hotmail.com>
2025-12-23 23:03:39 +0100

    Fixing deployment issuses

--- frontend/utils/get-strapi-url.ts
+++ frontend/utils/get-strapi-url.ts
@@ -1,6 +1,6 @@
 export function getStrapiURL() {
     if (typeof window === "undefined") {
-        return process.env.STRAPI_API_URL ?? "http://localhost:1337";
+        return process.env.STRAPI_API_URL;
     }
-    return process.env.NEXT_PUBLIC_STRAPI_API_URL ?? "http://localhost:1337";
+    return process.env.NEXT_PUBLIC_STRAPI_API_URL;
 }
